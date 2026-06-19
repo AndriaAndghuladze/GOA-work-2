75674a1bc8078db40474e92797847c25b36545f9
AndriaAndghuladze <andriaand999@gmail.com>
2026-06-19 20:07:37 +0400

    commit

---
+++ Level 175/classwork/codewars.py
@@ -0,0 +1,3 @@
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+def solution(number):
+    return sum(x for x in range(number) if x % 3 == 0 or x % 5 == 0)
